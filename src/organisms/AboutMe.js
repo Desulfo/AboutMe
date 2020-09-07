18b9e78eb09973f772dbf631d5289c23f9657e01
Pawel Janiszewski <puyol_11@interia.pl>
2020-09-07 12:26:56 +0200

    Add animation to arrow in MyProject component.

--- src/organisms/AboutMe.js
+++ src/organisms/AboutMe.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 import Section from '../atoms/Section';
 import Paragraph from '../atoms/basicTypography/Paragraph';
 import Header from '../atoms/basicTypography/Header';
 import Header4 from '../atoms/basicTypography/Header4';
 import Icon from '../atoms/FavIcon';
 import Link from '../atoms/basicTypography/Link';
+
+const rotate = keyframes`
+  50% {transform: rotate(-180deg); font-size: 2rem; margin-bottom:0}
+  100% {transform: rotate(0deg); font-size: 1.5rem}
+`;
+const AnimatedIcon = styled(Icon)`
+  &:hover {
+    animation: ${rotate} 2s linear;
+  }
+`;
+const AnimatedLink = styled(Link)``;
 
 function AboutMe() {
   return (
@@ -16,11 +29,14 @@ function AboutMe() {
         voluptatibus. Expedita sit alias nobis perspiciatis quam sint aut sed,
         inventore iusto magni eaque magnam deserunt nisi harum quas eos itaque!"
       </Paragraph>
-      <Link url="#Stack">
+      <AnimatedLink url="#Stack">
         My stack
         <br />
-        <Icon className="fas fa-arrow-circle-down" styles="margin: .5rem 0;" />
-      </Link>
+        <AnimatedIcon
+          className="fas fa-arrow-circle-down"
+          styles="margin: .5rem 0;"
+        />
+      </AnimatedLink>
     </Section>
   );
 }
