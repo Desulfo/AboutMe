89a8aabe89165c587014e9f7135c2a3a00f0daaa
Pawel Janiszewski <puyol_11@interia.pl>
2020-09-07 13:24:46 +0200

    Add animation to the button im Contact component.

--- src/atoms/styled/Contact/Button.js
+++ src/atoms/styled/Contact/Button.js
@@ -1,15 +1,43 @@
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 
 const Button = styled.button`
   width: 100px;
   height: 30px;
   font-weight: bold;
-  background-color: #f0f0f0;
   border-radius: 50px;
-  border: 1px solid black;
-  &:hover {
+  border: 0 solid black;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #f0f0f0;
+    border-radius: 10rem;
+    z-index: -2;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
     background-color: #364f6b;
+    transition: all 0.3s;
+    border-radius: 10rem;
+    z-index: -1;
+  }
+  &:hover {
     cursor: pointer;
+    &:before {
+      width: 100%;
+    }
   }
 `;
 
