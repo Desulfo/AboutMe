76ffb12b153f69154f1d5954c7bdc6d6e018488a
Pawel Janiszewski <puyol_11@interia.pl>
2020-09-20 18:33:31 +0200

    Correct styles for Prjects component

--- src/organisms/Projects.js
+++ src/organisms/Projects.js
@@ -1,14 +1,22 @@
 import React from 'react';
+
+import styled from 'styled-components';
 import Header from '../atoms/basicTypography/Header';
 import ProjectItem from '../molecules/ProjectItem';
 import Section from '../atoms/Section';
 
+const StyledSection = styled(Section)`
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+`;
+
 function Projects() {
   return (
-    <Section id="Projects">
+    <StyledSection id="Projects">
       <Header text="My projects" />
       <ProjectItem />
-    </Section>
+    </StyledSection>
   );
 }
 
